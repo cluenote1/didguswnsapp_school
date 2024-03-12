c22235f366d36d0a7e9df58f816c1bb777fb5e01
cluenote1 <hyeonjuny68@gmail.com>
2024-03-12 09:53:24 +0900

    Initial commit

---
+++ didguswnsapp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace didguswnsapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
