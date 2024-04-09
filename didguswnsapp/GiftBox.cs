403dc6a4f108d99ca4d20f875a5bcc9e50bd4ce0
cluenote1 <163072020+cluenote1@users.noreply.github.com>
2024-04-09 10:41:42 +0900

    혀 ㅇ변황

---
+++ didguswnsapp/GiftBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using didguswnsapp;
+
+namespace didguswnsapp
+{
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+        public ITEM_GRADE Grade;
+    }
+}
+
--- didguswnsapp/Program.cs
+++ didguswnsapp/Program.cs
@@ -1,64 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using GiftBoxManagerNamespace;
 
 namespace didguswnsapp
 {
-    class GiftBox
-    {
-        public string Letter;
-        public int Money;
-    }
-
+     
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            GiftBox addressA = GiftBoxMaker("A친구야 잘 지내지?", 110000);
-            GiftBox addressB = GiftBoxMaker("B친구야 잘 지내지?", 120000);
-            GiftBox addressC = GiftBoxMaker("C친구야 잘 지내지?", 130000);
+            
+            GiftBoxManager giftBoxManager = new GiftBoxManager();
+            ssgiftBoxManager.SetInit();
 
-            GiftBox[] giftBoxes = new GiftBox[3];
-            GiftBox giftBox = giftBoxes[0];
-            giftBoxes[0] = addressA;
-            giftBoxes[1] = addressB;
-            giftBoxes[2] = addressC;
-
-            Console.WriteLine("giftbox Arrajy Count : " + giftBoxes.Length);
-
-            //List~
-            List<GiftBox> giftBoxList = new List<GiftBox>();
-            giftBoxList.Add(addressA); //0
-            giftBoxList.Add(addressB); //1
-            giftBoxList.Add(addressC); //2
-
-            Console.WriteLine(giftBoxList[2].Letter);
-            Console.WriteLine("giftbox List Count : " + giftBoxList.Count);
-
-
-            for (int i = 0; i < giftBoxes.Length; i++)
-            {
-                giftBoxes[i].Letter = (i + 1) + "번째 편지";
-                Console.WriteLine(giftBoxes[i].Letter);
-            }
-
-            foreach (GiftBox gift in giftBoxes)
-            {
-                Console.WriteLine(gift.Letter);
-            }
-        }
-
-        
-
-        private static GiftBox GiftBoxMaker(string letter, int money)
-        {
-            GiftBox address = new GiftBox()
-            {
-
-                Letter = letter,
-                Money = money
-            };
-
-            return address;
 
         }
 
@@ -67,6 +20,5 @@ namespace didguswnsapp
     }
 }
 
-    
-    
+
 
