65ff07cc3811abd23b53b3121572c560dab59db3
cluenote1 <163072020+cluenote1@users.noreply.github.com>
2024-04-16 09:42:32 +0900

    value

--- didguswnsapp/Program.cs
+++ didguswnsapp/Program.cs
@@ -8,10 +8,24 @@ namespace didguswnsapp
 
         static void Main(string[] args)
         {
-            
-            GiftBoxManager giftBoxManager = new GiftBoxManager();
-            ssgiftBoxManager.SetInit();
+            Console.WriteLine("시작 부분");
 
+            int aMoney = 10000;
+            int bMoney = aMoney;
+            aMoney = 20000;
+            Console.WriteLine("aMoney:" + aMoney);
+            Console.WriteLine("bMoney:" + bMoney);
+           
+            GiftBox aGiftBox = new GiftBox()
+            {
+                Letter = "안녕",
+                Money = 10000,
+            };
+
+            GiftBox bGiftBox = aGiftBox;
+            bGiftBox.Money = 20000;
+            Console.WriteLine("aGiftBox:" + aGiftBox.Money);
+            Console.WriteLine("bGIftBox:" + bGiftBox.Money);
 
         }
 
